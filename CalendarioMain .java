91e18a81f3b0f10467bd2f95519c7672c39363de
ZeNeto7 <rodrigues.paiva@academico.ifpb.edu.br>
2025-05-21 10:34:42 -0300

    Update CalendarioMain .java

--- CalendarioMain .java
+++ CalendarioMain .java
@@ -1,7 +1,7 @@
 public class Calendariomain{
     public static void main(String[] args){
         Calendario c = new Calendario(25 ,11 ,2000);
-        Calendario d = new Calendario(12 ,06 ,2004);
+        Calendario d = new Calendario(12 ,6 ,2004);
 
         c.mostrarData();
         c.anoBissexto();
